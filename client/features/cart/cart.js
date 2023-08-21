d9a0ec8bf2e642df8c865d821fdf71a77a1c7dd6
Ivan Garcia <garcia.ivan1211@gmail.com>
2023-08-21 16:50:11 -0400

    Client folder cleaned up

--- client/features/cart/cart.js
+++ client/features/cart/cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getIncompleteOrders } from '../../store/ordersSlice';
 import { me } from '../auth/authSlice';
